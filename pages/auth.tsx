18d6c2f45416ea2c6ba02ccd2116728e4c089026
John_Lamy <johnclamy@gmail.com>
2023-05-03 23:15:40 +0100

    add a root bg img

---
+++ pages/auth.tsx
@@ -0,0 +1,7 @@
+export default function Auth() {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50"></div>
+    </div>
+  );
+}
